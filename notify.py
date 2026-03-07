b37891e0ff5be34aee3ee0594f12ff279b213d23
mkowusu0522-hash <mkowusu0522@gmail.com>
2026-03-06 21:07:12 -0500

    Add function to send text messages using SMTP
    Implement a function to send text messages via email.

---
+++ notify.py
@@ -0,0 +1,12 @@
+import smtplib
+
+def send_text(message: str):
+
+    sender = "mkowusu0522@gmail.com"
+    password = "xydrev-2fywzy-Fuwpuf"
+
+    receiver = "4438265706@txt.att.net"   # your phone via carrier gateway
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(sender, password)
+        server.sendmail(sender, receiver, message)
