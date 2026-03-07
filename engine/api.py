8cc01d695be815684c55d3f7dfd85f1cc425c74a
mkowusu0522-hash <mkowusu0522@gmail.com>
2026-03-06 20:09:12 -0500

    Refactor scan_market to read tickers from file

--- engine/api.py
+++ engine/api.py
@@ -10,7 +10,9 @@ def stock_judgment(ticker: str):
 
 @app.get("/scan")
 def scan_market():
-    from .scan import main
-    return main()
+    from .scan import read_tickers, TICKERS_FILE, scan_tickers
+    tickers = read_tickers(TICKERS_FILE)
+    return scan_tickers(tickers)
+
 
 
